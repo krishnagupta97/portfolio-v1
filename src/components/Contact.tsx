4da75b91dc6d785b8d71f85547a5df94b4c0f4b7
krishna <krishna.g@fnp.com>
2026-03-15 21:17:45 +0530

    minor bug fixes

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -60,7 +60,7 @@ const Contact = () => {
           </div>
           <div className="contact-box">
             <h2>
-              Designed and Developed <br /> by <span>Krishn Gupta</span>
+              Designed and Developed <br /> by <span>Krishna Gupta</span>
             </h2>
             <h5>
               <MdCopyright /> 2025
